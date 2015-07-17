0f25af80c2375a8f3511d7cb059a25acb4c6ae80
Travis Glenn Hansen <travisghansen@yahoo.com>
2015-07-17 08:28:23 -0600

    support -i option for testing alternative images ie: after sparsifying gentoo.img

--- launch.sh
+++ launch.sh
@@ -19,46 +19,46 @@ TOTAL_OPTIONS=0
 #MAX_PARAMETERS=1
 
 usage(){
-	echo "Usage: " "${SCRIPT_NAME} -t [build|test]";
+    echo "Usage: " "${SCRIPT_NAME} -t [build|test]";
 }
 
 echo_exec_info(){
-	echo "Script name: ${SCRIPT_NAME}"
-	echo "Script directory: ${SCRIPT_DIR}"
-	echo "Script path: ${SCRIPT_PATH}"
-	echo "Called as: ${CALLED_AS}"
-	echo "Called as full: ${CALLED_AS_FULL}"
-	echo "Called from: ${START_PWD}"
-	echo "Total options: ${TOTAL_OPTIONS}"
-	echo "Total parameters: ${TOTAL_PARAMETERS}"
-	echo "Total positional parameters: ${TOTAL_POSITIONAL_PARAMETERS}"
-	echo "Full options/parameter string: ${FULL_POSITIONAL_PARAMETER_STRING}"
+    echo "Script name: ${SCRIPT_NAME}"
+    echo "Script directory: ${SCRIPT_DIR}"
+    echo "Script path: ${SCRIPT_PATH}"
+    echo "Called as: ${CALLED_AS}"
+    echo "Called as full: ${CALLED_AS_FULL}"
+    echo "Called from: ${START_PWD}"
+    echo "Total options: ${TOTAL_OPTIONS}"
+    echo "Total parameters: ${TOTAL_PARAMETERS}"
+    echo "Total positional parameters: ${TOTAL_POSITIONAL_PARAMETERS}"
+    echo "Full options/parameter string: ${FULL_POSITIONAL_PARAMETER_STRING}"
 }
 
 parameter_validation(){
-	if [ -n "${MIN_OPTIONS}" ] && [ ${TOTAL_OPTIONS} -lt ${MIN_OPTIONS} ];then
-		echo "Not enough options"
-		usage
-		exit 1
-	fi
+    if [ -n "${MIN_OPTIONS}" ] && [ ${TOTAL_OPTIONS} -lt ${MIN_OPTIONS} ];then
+        echo "Not enough options"
+        usage
+        exit 1
+    fi
 
-	if [ -n "${MIN_PARAMETERS}" ] && [ ${TOTAL_PARAMETERS} -lt ${MIN_PARAMETERS} ];then
-		echo "Not enough parameters"
-		usage
-		exit 1
-	fi
+    if [ -n "${MIN_PARAMETERS}" ] && [ ${TOTAL_PARAMETERS} -lt ${MIN_PARAMETERS} ];then
+        echo "Not enough parameters"
+        usage
+        exit 1
+    fi
 
-	if [ -n "${MAX_OPTIONS}" ] && [ ${TOTAL_OPTIONS} -gt ${MAX_OPTIONS} ];then
-		echo "Too many options"
-		usage
-		exit 1
-	fi
+    if [ -n "${MAX_OPTIONS}" ] && [ ${TOTAL_OPTIONS} -gt ${MAX_OPTIONS} ];then
+        echo "Too many options"
+        usage
+        exit 1
+    fi
 
-	if [ -n "${MAX_PARAMETERS}" ] && [ ${TOTAL_PARAMETERS} -gt ${MAX_PARAMETERS} ];then
-		echo "Too many parameters"
-		usage
-		exit 1
-	fi
+    if [ -n "${MAX_PARAMETERS}" ] && [ ${TOTAL_PARAMETERS} -gt ${MAX_PARAMETERS} ];then
+        echo "Too many parameters"
+        usage
+        exit 1
+    fi
 }
 
 
@@ -76,11 +76,17 @@ main(){
     fi
 
     source config.sh
-    ./isogen.sh 
+    ./isogen.sh
+
+    if [ "x${launch_type}" == "xtest" ] || [ "x${launch_type}" == "xplayground" ];then
+        if [ "x${IMAGE}" != "x" ];then
+            IMG="${IMAGE}"
+        fi
+    fi
 
     qemu-system-x86_64 \
         -enable-kvm -m "${QEMU_MEMORY:-512}" -smp $(nproc) \
-        -drive file=gentoo.img,if=virtio,index=0 \
+        -drive file=${IMG},if=virtio,index=0 \
         -cdrom "iso/${ISO}" \
         -drive file="iso/builder.iso",media=cdrom \
         -drive file="iso/config.iso",media=cdrom \
@@ -96,40 +102,39 @@ main(){
 
 #Parse options/parameters
 while [ "$1" != "" ]; do
-	#Options should always come first before parameters
-	if [ "${1:0:1}" = "-" ];then
-		TOTAL_OPTIONS=$(($TOTAL_OPTIONS + 1))
-		case $1 in
-			-t | --type )			shift
-									launch_type=$1
-									;;
-			-i | --interactive )	interactive=1
-									;;
-			-h | --help )			usage
-									exit
-									;;
-			* )						echo "Invalid option: " ${1}
-									usage
-									exit 1
-		esac
-	#Parameters always at the end
-	else
-		TOTAL_PARAMETERS=$#
-		#FILE=$1
-		#FILE2=$3
-		#...
-		#Clean them all up if desired
-		while [ "$1" != "" ]; do
-			shift;
-		done
-		break;
-	fi
-	shift
+    #Options should always come first before parameters
+    if [ "${1:0:1}" = "-" ];then
+        TOTAL_OPTIONS=$(($TOTAL_OPTIONS + 1))
+        case $1 in
+            -t | --type )           shift
+                                    launch_type=$1
+                                    ;;
+            -i | --image )          shift
+                                    IMAGE=$1
+                                    ;;
+            -h | --help )           usage
+                                    exit
+                                    ;;
+            * )                     echo "Invalid option: " ${1}
+                                    usage
+                                    exit 1
+        esac
+    #Parameters always at the end
+    else
+        TOTAL_PARAMETERS=$#
+        #FILE=$1
+        #FILE2=$3
+        #...
+        #Clean them all up if desired
+        while [ "$1" != "" ]; do
+            shift;
+        done
+        break;
+    fi
+    shift
 done
 
 
 #echo_exec_info
 parameter_validation
 main
-
-
